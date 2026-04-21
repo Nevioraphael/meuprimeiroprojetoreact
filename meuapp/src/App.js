0ed393ad3c90faeb01ae98e67ef19c2e2612cec8
Nevio Raphael <nevioraphael@outlook.com>
2026-04-21 13:29:03 -0300

    alterações no projeto

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editando essa página <code>src/App.js</code> and save to reload.
+          Editando essa página <code>src/App.js</code> e salvando pra começar.
         </p>
         <a
           className="App-link"
